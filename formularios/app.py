2677b9d9fcfa6b1d02fd080cad3d567286e5634e
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-05-17 20:03:14 -0300

    introducao formularios

--- formularios/app.py
+++ formularios/app.py
@@ -11,9 +11,12 @@ def index():
     chocolate = request.form.get('chocolate')
     coquinha = request.form.get('coquinha')
     sexo = request.form.get('sexo')
+    cores = request.form.get('cores')
+    texto = request.form.get('texto')
     return f"<h1>{usuario}</h1><h1>{senha}</h1> \
         {datanascimento}Chocolate: {chocolate} \
-        Coquinha: {coquinha} Sexo: {sexo}"
+        Coquinha: {coquinha} Sexo: {sexo} \
+        Cores: {cores} Texto: {texto}" 
 
 @app.route("/formulario")
 def formulario():
@@ -56,6 +59,17 @@ def formulario():
                 <input type="text" name="textofixo" 
                 value="ta vendo q nao altera"
                 readonly="readonly" disabled>
+            </fieldset>
+            <fieldset>
+                <select name="cores">
+                    <option value="1">Azul</option>
+                    <option value="2">Verde</option>
+                    <option value="3">Vermelho</option>
+                </select>
+            </fieldset>
+            <fieldset>
+                <textarea rows="10" 
+                cols="30" name="texto"></textarea> 
             </fieldset>
             <input type="submit">
             <!--
