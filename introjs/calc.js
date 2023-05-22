59ef021e53bfc91852b019025bf8ede7e55d08ed
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-05-22 20:27:59 -0300

    intro Javascript

---
+++ introjs/calc.js
@@ -0,0 +1,6 @@
+
+var refdisplay = document.getElementById('display')
+function botaoClicado(numero){
+    console.log(numero)
+    refdisplay.value += numero
+}
---
+++ introjs/introjs.js
@@ -0,0 +1,14 @@
+//alert("passou no arquivo")
+
+/*var resposta = confirm("Voce esta certo disso?")
+alert("Sua resposta foi: " + resposta)*/
+
+//var nome = prompt("Digite seu nome")
+//contatenação de variaveis 
+//alert("Seu nome e" + nome)
+document.write("EU NAO ACREDITO QUE O JS ALTERA O HTML")
+document.title = "MUDANDO A ABA DA JANELA"
+
+function meuCodigo(){
+    document.write("Meu codigo")
+}
