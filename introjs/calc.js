0bb18898a320f2956f091ddee7026874f46860bd
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-05-31 19:05:58 -0300

    conclusao da calculadora

--- introjs/calc.js
+++ introjs/calc.js
@@ -4,3 +4,14 @@ function botaoClicado(numero){
     console.log(numero)
     refdisplay.value += numero
 }
+function resultado(){
+    refdisplay.value = eval(refdisplay.value)
+}
+function limpar(){
+    refdisplay.value = " "
+}
+function backspace(){
+    console.log("Tamanho: " + refdisplay.value.length)
+    refdisplay.value = refdisplay.value.substring(0,refdisplay.value.length-1)
+}
+/*refdisplay.value += " " */
