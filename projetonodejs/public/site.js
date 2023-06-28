b0a7a8ea5525248dcc6f32874d143a17e69d5228
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-06-28 20:28:47 -0300

    CRUD: Delete

--- projetonodejs/public/site.js
+++ projetonodejs/public/site.js
@@ -1,5 +1,20 @@
 (function(){
     $('#clientes').on('click','.js-delete',function(){
-        
+        let botaoclicado = $(this) //recuperar o botao
+        $('#btnsim').attr('data-id',botaoclicado.attr('data-id'))
+        $('#meumodal').modal('show')//mostra a janela de confirmação
+    })
+
+    $('#btnsim').on('click', function(){
+        let botaosim = $(this)//recuperando a referencia do botao sim
+        let codcliente = botaosim.attr('data-id') //recuperando cod cliente
+        //chamada para o backend apagar o cliente
+        $.ajax({
+            url: '/clientes/delete/' + codcliente,
+            method: 'GET',
+            success: function(){
+                window.location.href = '/clientes'
+            }
+        })
     })
 })()
